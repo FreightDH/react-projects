01118c6f445d998e47d4043c93f34a3fff444596
FreightDH <maxim.zolotkov@yandex.ru>
2023-10-22 21:40:55 +0300

    feat: add app component

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,14 +1,21 @@
+import AddSection from './components/AddSection/AddSection';
+import TasksSection from './components/TasksSection/TasksSection';
+import styles from './styles/Page.module.scss';
 import './styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <main className="page">
+    <>
+      <main className={styles.page}>
         <div className="page__container">
-          <div className="page__body"></div>
+          <div className={styles.page__body}>
+            <div className={styles.page__title}>todos</div>
+            <AddSection />
+            <TasksSection />
+          </div>
         </div>
       </main>
-    </div>
+    </>
   );
 }
 
