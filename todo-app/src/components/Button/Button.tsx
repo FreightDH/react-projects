19878ac014166c4072d7dfc86a756432161dd7a1
FreightDH <maxim.zolotkov@yandex.ru>
2023-10-22 21:34:34 +0300

    feat: create button component

---
+++ todo-app/src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import { ComponentPropsWithRef, ReactElement } from 'react';
+import styles from './Button.module.scss';
+
+interface ButtonProps extends ComponentPropsWithRef<'button'> {
+  action: 'add' | 'complete' | 'edit' | 'delete';
+}
+
+const Button: React.FC<ButtonProps> = ({ action, children, onClick, ...props }): ReactElement => {
+  const classes = `${styles.button} ${styles[action]}`;
+
+  return (
+    <button className={classes} onClick={onClick} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
