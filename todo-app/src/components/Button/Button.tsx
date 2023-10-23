c71ec85baae72caa895afdfb6fe99d75029c9b86
FreightDH <maxim.zolotkov@yandex.ru>
2023-10-23 11:25:00 +0300

    feat: add new button type

--- todo-app/src/components/Button/Button.tsx
+++ todo-app/src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import { ComponentPropsWithRef, ReactElement } from 'react';
 import styles from './Button.module.scss';
 
 interface ButtonProps extends ComponentPropsWithRef<'button'> {
-  action: 'add' | 'complete' | 'edit' | 'delete';
+  action: 'add' | 'complete' | 'edit' | 'delete' | 'save';
 }
 
 const Button: React.FC<ButtonProps> = ({ action, children, onClick, ...props }): ReactElement => {
