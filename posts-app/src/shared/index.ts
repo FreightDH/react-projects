85e0373d9c257a48f55088c7cda55c0f2f7a323d
FreightDH <maxim.zolotkov@yandex.ru>
2023-11-21 14:46:18 +0300

    feat: add CustomLink component

--- posts-app/src/shared/index.ts
+++ posts-app/src/shared/index.ts
@@ -1,5 +1,6 @@
+import CustomLink from './components/CustomLink/CustomLink';
 import FormButton from './components/FormButton/FormButton';
 import FormInput from './components/FormInput/FormInput';
 import { isEmpty } from './functions/validation';
 
-export { FormButton, FormInput, isEmpty };
+export { CustomLink, FormButton, FormInput, isEmpty };
